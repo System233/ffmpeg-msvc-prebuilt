9384c8198eba53c814e8ac03009da3c170c7d462
System233 <cdkeylzh@outlook.com>
2024-11-19 01:38:14 +0800

    rebuild

--- build.sh
+++ build.sh
@@ -26,11 +26,12 @@ echo FF_ARGS=$FF_ARGS
 ./build-make-dep.sh nv-codec-headers
 
 ./build-cmake-dep.sh zlib
-./build-cmake-dep.sh openexr -DOPENEXR_INSTALL_TOOLS=OFF
 ./build-cmake-dep.sh freetype
-./build-cmake-dep.sh harfbuzz
+./build-cmake-dep.sh harfbuzz -DHB_HAVE_FREETYPE=ON
 
 ./build-cmake-dep.sh SDL
-./build-cmake-dep.sh libjxl -DBUILD_TESTING=OFF -DJPEGXL_ENABLE_BENCHMARK=OFF -DJPEGXL_ENABLE_JNI=OFF -DJPEGXL_BUNDLE_LIBPNG=OFF -DJPEGXL_ENABLE_TOOLS=OFF -DJPEGXL_ENABLE_EXAMPLES=OFF #-DJPEGXL_STATIC=ON
 
-./build-ffmpeg.sh FFmpeg --enable-libfreetype --enable-libharfbuzz --enable-sdl --enable-libjxl $FF_ARGS
+#./build-cmake-dep.sh openexr -DOPENEXR_INSTALL_TOOLS=OFF
+#./build-cmake-dep.sh libjxl -DBUILD_TESTING=OFF -DJPEGXL_ENABLE_BENCHMARK=OFF -DJPEGXL_ENABLE_JNI=OFF -DJPEGXL_BUNDLE_LIBPNG=OFF -DJPEGXL_ENABLE_TOOLS=OFF -DJPEGXL_ENABLE_EXAMPLES=OFF #-DJPEGXL_STATIC=ON
+
+./build-ffmpeg.sh FFmpeg --enable-libfreetype --enable-libharfbuzz --enable-sdl $FF_ARGS #--enable-libjxl $FF_ARGS
