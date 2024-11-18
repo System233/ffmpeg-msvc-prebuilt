2a1428686b8694caf6f5eb911e2c971f61999135
System233 <cdkeylzh@outlook.com>
2024-11-18 10:09:01 +0800

    测试meson build

--- build.sh
+++ build.sh
@@ -7,13 +7,14 @@
 set -e
 source ./env.sh
 
-./build-make-dep.sh nv-codec-headers
-./build-cmake-dep.sh zlib
-./build-cmake-dep.sh SDL
-./build-cmake-dep.sh libjxl -DBUILD_TESTING=OFF -DJPEGXL_ENABLE_BENCHMARK=OFF -DJPEGXL_ENABLE_JNI=OFF -DJPEGXL_BUNDLE_LIBPNG=OFF -DJPEGXL_ENABLE_TOOLS=OFF -DJPEGXL_ENABLE_JPEGLI=OFF -DJPEGXL_ENABLE_EXAMPLES=OFF -DJPEGXL_STATIC=ON
 ./build-cmake-dep.sh freetype
 ./build-cmake-dep.sh harfbuzz
 ./build-meson-dep.sh fribidi
 ./build-meson-dep.sh libass
+
+./build-make-dep.sh nv-codec-headers
+./build-cmake-dep.sh zlib
+./build-cmake-dep.sh SDL
+./build-cmake-dep.sh libjxl -DBUILD_TESTING=OFF -DJPEGXL_ENABLE_BENCHMARK=OFF -DJPEGXL_ENABLE_JNI=OFF -DJPEGXL_BUNDLE_LIBPNG=OFF -DJPEGXL_ENABLE_TOOLS=OFF -DJPEGXL_ENABLE_JPEGLI=OFF -DJPEGXL_ENABLE_EXAMPLES=OFF -DJPEGXL_STATIC=ON
 
 ./build-ffmpeg.sh FFmpeg --enable-libfreetype --enable-libass --enable-sdl --enable-libjxl
