47347795e83505f2aecc3dbb4a7b2b6e11f60ba1
System233 <cdkeylzh@outlook.com>
2024-11-18 10:18:17 +0800

    meson BUILD

--- build-meson-dep.cmd
+++ build-meson-dep.cmd
@@ -1,6 +1,7 @@
 @echo off
 
 SET PATH="%ORIGINAL_PATH%;%MSYSTEM_PREFIX%\bin"
+SET MESON_CMD=python3 -m meson
 CD /D "%BUILD_DIR%"
-meson setup "%SRC_DIR%" --vsenv  --prefix "%INSTALL_PREFIX%" %*&&meson compile -C .&&meson install -C .
+%MESON_CMD% setup "%SRC_DIR%" --vsenv  --prefix "%INSTALL_PREFIX%" %*&&%MESON_CMD% compile -C .&&%MESON_CMD% install -C .
 EXIT %ERRORLEVEL%
