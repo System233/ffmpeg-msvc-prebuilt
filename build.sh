9f9fee9e0673c412cda74bed8fc7b52375df194d
System233 <cdkeylzh@outlook.com>
2024-11-19 01:14:07 +0800

    rebuild

--- build.sh
+++ build.sh
@@ -27,8 +27,8 @@ echo FF_ARGS=$FF_ARGS
 
 ./build-cmake-dep.sh zlib
 ./build-cmake-dep.sh openexr -DOPENEXR_INSTALL_TOOLS=OFF
-./build-cmake-dep.sh harfbuzz
 ./build-cmake-dep.sh freetype
+./build-cmake-dep.sh harfbuzz
 
 ./build-cmake-dep.sh SDL
 ./build-cmake-dep.sh libjxl -DBUILD_TESTING=OFF -DJPEGXL_ENABLE_BENCHMARK=OFF -DJPEGXL_ENABLE_JNI=OFF -DJPEGXL_BUNDLE_LIBPNG=OFF -DJPEGXL_ENABLE_TOOLS=OFF -DJPEGXL_ENABLE_EXAMPLES=OFF #-DJPEGXL_STATIC=ON
