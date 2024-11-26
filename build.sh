6a809b47cd391fd9cc3dd34aaa3fcc01bcedb184
System233 <cdkeylzh@outlook.com>
2024-11-26 10:38:47 +0800

    yasm

--- build.sh
+++ build.sh
@@ -122,7 +122,7 @@ if [ -n "$ENABLE_LIBVPX" ]; then
     fi
     apply-patch libvpx libvpx.patch
     export
-    AR=lib ARFLAGS= CC=cl CXX=cl LD=link STRIP=false target= ./build-make-dep.sh libvpx --target=$libvpx_target --as=yasm --disable-optimizations --disable-dependency-tracking --disable-runtime-cpu-detect --disable-thumb --disable-neon --enable-external-build $LIBVPX_ARGS
+    AS=yasm AR=lib ARFLAGS= CC=cl CXX=cl LD=link STRIP=false target= ./build-make-dep.sh libvpx --target=$libvpx_target --as=yasm --disable-optimizations --disable-dependency-tracking --disable-runtime-cpu-detect --disable-thumb --disable-neon --enable-external-build $LIBVPX_ARGS
     FF_ARGS=--enable-libvpx
 fi
 ./build-ffmpeg.sh FFmpeg $FF_ARGS
