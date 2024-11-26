4b93709519341fc4788dd316c534e6ad56a32168
System233 <cdkeylzh@outlook.com>
2024-11-26 10:23:19 +0800

    test

--- build.sh
+++ build.sh
@@ -121,7 +121,7 @@ if [ -n "$ENABLE_LIBVPX" ]; then
         LIBVPX_ARGS="--enable-static-msvcrt"
     fi
     apply-patch libvpx libvpx.patch
-    AR=lib ARFLAGS= CC=cl CXX=cl LD=link STRIP=false ./build-make-dep.sh libvpx --target=$libvpx_target --as=yasm --disable-optimizations --disable-dependency-tracking --disable-runtime-cpu-detect --disable-thumb --disable-neon --enable-external-build $LIBVPX_ARGS
+    AR=lib ARFLAGS= CC=cl CXX=cl LD=link STRIP=false TARGET= ./build-make-dep.sh libvpx --target=$libvpx_target --as=yasm --disable-optimizations --disable-dependency-tracking --disable-runtime-cpu-detect --disable-thumb --disable-neon --enable-external-build $LIBVPX_ARGS
     FF_ARGS=--enable-libvpx
 fi
 ./build-ffmpeg.sh FFmpeg $FF_ARGS
