e3b138d260a17f59b4a9d21d39f16017231c9b0e
System233 <cdkeylzh@outlook.com>
2024-12-04 22:37:39 +0800

    retest

--- build-libass.sh
+++ build-libass.sh
@@ -12,7 +12,7 @@ export PATH=$PATH:$GNULIB_SRCDIR
 
 echo -e "\n[Build fribidi]"
 cd $SRC_DIR/fribidi
-./autogen.sh
+NOCONFIGURE=1 ./autogen.sh
 ./configure "--host=${BUILD_ARCH}-windows" --prefix=$INSTALL_PREFIX --disable-shared --enable-static --disable-dependency-tracking 
 make -C lib install -j$(nproc) CFLAGS="$CFLAGS -DHAVE_STRINGIZE" 
 make install-data-am
@@ -20,7 +20,7 @@ make install-data-am
 
 echo -e "\n[Build libass]"
 cd $SRC_DIR/libass
-./autogen.sh
+NOCONFIGURE=1 ./autogen.sh
 ./configure "--host=${BUILD_ARCH}-windows" --prefix=$INSTALL_PREFIX --disable-shared --enable-static  --disable-asm --disable-dependency-tracking
 make install -j$(nproc)
 make install-data-am
