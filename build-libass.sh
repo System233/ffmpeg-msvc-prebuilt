318f108c0ed4d26e0884e45d7c6ed795c5f732b8
System233 <cdkeylzh@outlook.com>
2024-12-04 22:26:39 +0800

    test

--- build-libass.sh
+++ build-libass.sh
@@ -4,7 +4,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-set -e
+set -ex
 SRC_DIR=$(pwd)
 export GNULIB_SRCDIR="$SRC_DIR/gnutls/gnulib"
 export PATH=$PATH:$GNULIB_SRCDIR
@@ -12,7 +12,7 @@ export PATH=$PATH:$GNULIB_SRCDIR
 echo -e "\n[Build fribidi]"
 cd $SRC_DIR/fribidi
 ./autogen.sh
-./configure --host=${BUILD_ARCH}-windows --prefix=$INSTALL_PREFIX --disable-shared --enable-static --disable-dependency-tracking 
+./configure "--host=${BUILD_ARCH}-windows" --prefix=$INSTALL_PREFIX --disable-shared --enable-static --disable-dependency-tracking 
 make -C lib install -j$(nproc) CFLAGS="$CFLAGS -DHAVE_STRINGIZE" 
 make install-data-am
 
@@ -20,7 +20,7 @@ make install-data-am
 echo -e "\n[Build libass]"
 cd $SRC_DIR/libass
 ./autogen.sh
-./configure --host=${BUILD_ARCH}-windows --prefix=$INSTALL_PREFIX --disable-shared --enable-static  --disable-asm --disable-dependency-tracking
+./configure "--host=${BUILD_ARCH}-windows" --prefix=$INSTALL_PREFIX --disable-shared --enable-static  --disable-asm --disable-dependency-tracking
 make install -j$(nproc)
 make install-data-am
 
