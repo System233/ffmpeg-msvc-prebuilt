5f163088d3abc252552b4d1d816133ee053005a3
System233 <cdkeylzh@outlook.com>
2024-12-04 23:34:46 +0800

    ts

--- build-gnutls.sh
+++ build-gnutls.sh
@@ -3,16 +3,18 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-set -e
+set -ex
 SRC_DIR=$(pwd)
 export GNULIB_SRCDIR=$SRC_DIR/gnutls/gnulib
 export PATH=$PATH:$GNULIB_SRCDIR
 
+echo -e '[Build gmplib]'
 ./build-make-dep.sh gmplib --host=${BUILD_ARCH}-windows --prefix=$INSTALL_PREFIX --disable-shared --enable-static --disable-assembly gmp_cv_asm_w32=.word
-# mv $INSTALL_PREFIX/lib/libgmp.a $INSTALL_PREFIX/lib/gmp.lib
+mv $INSTALL_PREFIX/lib/libgmp.a $INSTALL_PREFIX/lib/gmp.lib
 
 
 
+echo -e '[Build nettle]'
 cd $SRC_DIR/gnutls/devel/nettle
 
 gnulib-tool --import unistd
@@ -23,13 +25,14 @@ autoconf
 
 make -j$(nproc) install
 
+echo -e '[Build libtasn1]'
 cd $SRC_DIR/gnutls/devel/libtasn1
 ./bootstrap
 ./configure --host=${BUILD_ARCH}-windows --prefix=$INSTALL_PREFIX --disable-shared --enable-static  --disable-valgrind-tests  --disable-doc   --disable-dependency-tracking CFLAGS=-DASN1_STATIC
 make -C lib install
 # mv $INSTALL_PREFIX/lib/libtasn1.a $INSTALL_PREFIX/lib/tasn1.lib
 
-
+echo -e '[Build libev]'
 cd $SRC_DIR/libev
 gnulib-tool --import sys_time
 autoconf
@@ -39,6 +42,7 @@ make install -j$(nproc)
 
 
 
+echo -e '[Build gnutls]'
 cd $SRC_DIR/gnutls
 # find . -name "*.gperf"|xargs -P$(nproc) -I{} sed -i "s/\r//g" '{}'
 gnulib-tool --add-import dirent opendir closedir readdir --local-dir=gl
